44f9798480c5595d88ea8851b4cabccc6ae9a8b5
mdalamin <alamin.md95@yahoo.com>
2020-11-24 12:26:47 -0500

    i am adding new class

--- GroupOneProject/src/test/java/com/osa/fileReaders/ExcelFileRead.java
+++ GroupOneProject/src/test/java/com/osa/fileReaders/ExcelFileRead.java
@@ -1,5 +1,37 @@
 package com.osa.fileReaders;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+
 public class ExcelFileRead {
+	@DataProvider(name = "FB")
+	public Object[][] excelRead() throws IOException {
+		
+		FileInputStream fl = new FileInputStream("/Users/mdalamin/Desktop/Book01.xlsx");
+		
+		XSSFWorkbook workbook = new XSSFWorkbook (fl);
+		XSSFSheet sheet = workbook.getSheet("Sheet1");
+		        int rowSize = sheet.getLastRowNum();
+		        int  cellSize= sheet.getRow(0).getLastCellNum();
+		        
+		    Object[][]data = new Object[rowSize][cellSize];
+		        
+		      for (int i=1; i<=rowSize; i++) {
+		      for(int j=0; j<cellSize; j++) {
+		    	  
+		    	data[i-1][j] = sheet.getRow(i).getCell(j); 
+	
+		    	
+	    }  
+		    	  
+      }	  
+		    	  
+		    return data; 	  
+		       
+	}
 
 }
