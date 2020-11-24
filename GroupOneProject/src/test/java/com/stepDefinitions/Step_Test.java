9df8a9515f51c4b0f2edc10f62c39363ce245660
MD AL AMIN <alamin.md95@yahoo.com>
2020-11-24 13:15:09 -0500

    i am adding new class

---
+++ GroupOneProject/src/test/java/com/stepDefinitions/Step_Test.java
@@ -0,0 +1,60 @@
+package com.stepDefinitions;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Step_Test {
+	
+	@Given("^I want to use this template for my feature file$")
+	
+	public void i_want_to_use_this_template_for_my_feature_file() {
+
+}
+	@And("^some other precondition$") 
+	public void some_other_precondition() {
+			
+		}
+	@When ("^I complete action$") 
+	public void i_complete_action() {
+		
+	}
+	@And ("^some other action$")
+	public void some_other_action () {
+		
+	}
+	@And ("^yet another action$")
+	public void yet_another_action() {
+		
+	}
+	@Then("^I validate the outcomes$")
+	public void i_validate_the_outcomes() {
+		
+	}
+	@And ("^check more outcomes$")
+	public void check_more_outcomes() {
+		
+	}
+
+	@Given("^I want to write a step with precondition$")
+	public void i_want_to_write_a_step_with_precondition() throws Throwable {
+		
+	}
+	@Given("^I want to write a step with name(\\d+)$")
+	public void i_want_to_write_a_step_with_name(int arg1) throws Throwable {
+		
+	}
+	@When("^I check for the (\\d+) in step$")
+	public void i_check_for_the_in_step(int arg1) throws Throwable {
+		
+	}
+	@Then("^I verify the success in step$")
+	public void i_verify_the_success_in_step() throws Throwable {
+		
+	}
+	@Then("^I verify the Fail in step$")
+	public void i_verify_the_Fail_in_step() throws Throwable {
+		
+	}
+	}
